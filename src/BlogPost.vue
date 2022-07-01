abecf1d18706b18cba30993e9bb3059ba9bb2655
Xieth <17730323876@163.com>
2022-07-01 18:35:52 +0800

    代码提交

---
+++ src/BlogPost.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="blog-post">
+    <h4>{{ title }}</h4>
+    <button @click="$emit('enlarge-text')">Enlarge text</button>
+    <strong>This is an Error for Demo Purposes</strong>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BlogPost",
+  props: ['title'],
+  setup (props) {
+    console.log(props)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
